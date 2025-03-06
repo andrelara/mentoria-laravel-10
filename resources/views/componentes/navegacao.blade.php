bf888337bda9cd7b9c8bf694b9e454e0b08a16ab
root <root@DESKTOP-JKUO58C.>
2025-03-06 09:22:06 -0300

    envio

---
+++ resources/views/componentes/navegacao.blade.php
@@ -0,0 +1,39 @@
+<ul class="nav flex-column">
+    <li class="nav-item">
+      <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
+        <svg class="bi"><use xlink:href="#house-fill"/></svg>
+        Dashboard
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link d-flex align-items-center gap-2" href="#">
+        <svg class="bi"><use xlink:href="#file-earmark"/></svg>
+        Pacientes
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link d-flex align-items-center gap-2" href="#">
+        <svg class="bi"><use xlink:href="#cart"/></svg>
+        Exames
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link d-flex align-items-center gap-2" href="#">
+        <svg class="bi"><use xlink:href="#people"/></svg>
+        Laudos
+      </a>
+    </li>
+
+  </ul>
+
+
+  <hr class="my-3">
+
+  <ul class="nav flex-column mb-auto">
+    <li class="nav-item">
+      <a class="nav-link d-flex align-items-center gap-2" href="#">
+        <svg class="bi"><use xlink:href="#door-closed"/></svg>
+        SAIR
+      </a>
+    </li>
+  </ul>
